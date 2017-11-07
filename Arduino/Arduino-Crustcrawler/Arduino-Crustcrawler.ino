f210196092ddb5e2605792452a667a3bc2607c2b
Jón Davíð <guni_29@hotmail.com>
2017-11-07 15:36:08 +0100

    Add files via upload

--- Arduino/Arduino-Crustcrawler/Arduino-Crustcrawler.ino
+++ Arduino/Arduino-Crustcrawler/Arduino-Crustcrawler.ino
@@ -3,7 +3,14 @@
     a crustcrawler manipulator with emg signals.
 
 */
+/*********************
+For Debugging
+*********************/
+//#define DEBUG
+//#ifdef DEBUG
+//#endif DEBUG
 
+#include <math.h>
 // Include the necessary ros packages
 #include <ros.h>
 #include <std_msgs/UInt16MultiArray.h>
@@ -13,7 +20,8 @@
 #include "Dynamixel_Serial.h"
 #include <SoftwareSerial.h>
 
-#include <math.h>
+// Include IRremote library to play around
+#include <IRremote.h>
 
 // Define parameters
 #define SERVO_ControlPin 0x02       // Control pin of buffer chip, NOTE: this does not matter becasue we are not using a half to full contorl buffer.
@@ -21,131 +29,43 @@
 #define LED13 0x0D
 #define CW_LIMIT_ANGLE 0x001        // lowest clockwise angle is 1, as when set to 0 it set servo to wheel mode
 #define CCW_LIMIT_ANGLE 0xFFF       // Highest anit-clockwise angle is 0XFFF, as when set to 0 it set servo to wheel mode
+#define receiver 12
+
+//Define buttons on the IR remote
+#define POWER 1
+#define VOLP 2
+#define FUNC 3
+#define LEFT 4
+#define PAUSE 5
+#define RIGHT 6
+#define DOWN 7
+#define VOLM 8
+#define UP 9
+#define ZERO 10
+#define EQ 11
+#define REPEAT 12
+#define ONE 13
+#define TWO 14
+#define THREE 15
+#define FOUR 16
+#define FIVE 17
+#define SIX 18
+#define SEVEN 19
+#define EIGHT 20
+#define NINE 21
+#define ON true
+
+//Global Variable for buttons
+int button_pressed;
+boolean power = ON;
+//Setting up the cartesian coordinates for initial position
+double x = 48.4, y = 0, z = 17.7;
 
 // Set the serial pins
+IRrecv irrecv(receiver);
+decode_results results;
 SoftwareSerial mySerial( 10 , 11 );    // RX, TX
 
-// Input is X, Y, Z
-
-void invKin(double X, double Y, double Z){
-  double base = 17.66;
-  double link1 = 21.98;
-  double link2 = 26.428;
-
-  // Vector used in q1
-  double V1[3] = {0, 0, 1};
-  // Second vector used in q1
-  double V2[3] = {X, Y, Z-base};
-  // Dot product in q1
-  double C;
-  for(int i=0; i<3; i++){
-    C += V1[i]+V2[i];
-  }
-  // The square root thing
-  double S = sqrt((pow(X, 2)+pow(Y, 2)+pow((Z-base), 2)));
-  // Deriving the second ange through q1 and q2
-  double q1 = (PI/2)-acos(C/S);
-  double q2 = acos((pow(link1, 2)-pow(link2, 2)+pow(S, 2)));
-  // Deriving the third angle
-  double q3 = acos((pow(link2, 2)+pow(link1, 2)-pow(S, 2)));
-  // Deriving the first angle
-  double theta1 = atan2(Y, X);
-  // Deriving the second angle
-  double theta2 = q1+q2;
-  // Deriving the third angle
-  double theta3 = q3-PI;
-
-  // Find the angles in degrees
-  double ang1 = (theta1*180)/PI+90;
-  double ang2 = (theta2*180)/PI+90;
-  double ang3 = (theta3*180)/PI+90;
-
-  double pos[3];
-  pos[0] = map(ang1, 0, 360, 0, 4095);
-  pos[1] = map(ang2, 0, 360, 0, 4095);
-  pos[2] = map(ang3, 0, 360, 0, 4095);
-
-  // return(&pos);
-}
-
-
-
-
-void emg_move_pos(const std_msgs::UInt16MultiArray& emg){
-    int emg_array[8];
-
-    for(int i=0; i<8; i++){
-      emg_array[i] = emg.data[i];
-    }
-    // Now we have the EMG data, lets convert them into x,y,z cartesian coordinates
-    // EMG 2 is right hand outwards, that is if EMG is highest value change substract it from y
-
-
-    }
-
-// This function(moveEmg) was used to move the robot by setting goal positions with the
-// EMG data coming in, when there were multiple joints being controlled there
-// was a lot of cross talk, the jooints were not easilly controlled
-void moveEmg(){
-  // Move right EMG 2
-    int emg_array[8];
-  // EMG 2 is right (+) and EMG 6 is left (-)
-    int emgDiff1 = emg_array[2]-emg_array[6];
-    int currPos1 = getpos(1);
-    int goalPos1 = currPos1 + emgDiff1;
-    // Serial.println(goalPos1);
-
-    int emgDiff2 = (emg_array[3]-emg_array[1]);
-    int currPos2 = getpos(2);
-    int goalPos2 = currPos2+emgDiff2;
-    // Serial.println(goalPos2);
-    int goalPos5, goalPos4;
-    if(emg_array[0] && emg_array[1] > 500){
-      int emgDiff4 = (emg_array[0]-emg_array[1]);
-      int currPos4 = getpos(4);
-      goalPos4 = currPos4 + emgDiff4;
-      goalPos5 = getpos(5) - emgDiff4;
-    }
-    // Serial.println(goalPos2);
-
-    // Move left EMG 6
-
-    // // Move up EMG 3
-
-    // // Move down EMG 1
-
-    // // Close / open gripper
-    // int goalPos5;
-    // if(emg_array[0] && emg_array[1] > 500){
-    //   int goalDeg5 = map(emg_array[1], 0, 2048, 0, 360);
-    //   goalPos5 = map(goalDeg5, 0, 360, 0, 4095)+1000;
-    // }
-    //
-    move(goalPos1, 3300, 1500 , 2045, 2045);
-}
-
-
-// Create a nodehandle for our subscriber
-ros::NodeHandle  nh;
-ros::Subscriber<std_msgs::UInt16MultiArray> sub("emg_data/mat_emg", emg_move_pos);
-
-void setup(){
-  // Initialise the node and subscriber
-  nh.initNode();
-  nh.subscribe(sub);
-
-  Serial.flush();                                       // Clear the serial buffer of garbage data before running the code.
-  mySerial.begin(SERVO_SET_Baudrate);                   // We now need to set Ardiuno to the new Baudrate speed 115200
-  Serial.begin(57600);                                  // Start serial communication on baudrate 57600
-  Dynamixel.begin(mySerial);                            // Calling mySerial function which sets 10 pin as the 2nd RX serial pin, and sets pin 11 as the 2nd TX serial pin
-  Dynamixel.setDirectionPin(SERVO_ControlPin);
-  // Turn on holding torque.
-  set_torque(true);
-  // Set the Profile acceleration.
-  set_profVel(10);
-  // Set the Profile velocity.
-  set_profAcc(10);
-}
 
 void serial_setup(int BAUD){
 
@@ -171,68 +91,225 @@ void set_torque(bool OnOff){
   Dynamixel.setHoldingTorque(0x05, OnOff);               //Turn on hold torque on servo 5
 }
 
+void begin_serial(){
+  Serial.flush();                                       // Clear the serial buffer of garbage data before running the code.
+  Serial.print("Starting up");
+  delay(100);
+  Serial.print("."); delay(100); Serial.print("."); delay(100); Serial.println(".");
+  mySerial.begin(SERVO_SET_Baudrate);                   // We now need to set Ardiuno to the new Baudrate speed 115200
+  Dynamixel.begin(mySerial);                            // Calling mySerial function which sets 10 pin as the 2nd RX serial pin, and sets pin 11 as the 2nd TX serial pin
+  Dynamixel.setDirectionPin(SERVO_ControlPin);
+  // Turn on holding torque.
+  set_torque(true);
+  // Set the Profile acceleration.
+  set_profAcc(10);
+  // Set the Profile velocity.
+  set_profVel(10);
+  Serial.println("Moving CrustCrawler into initial position...");
+  int myPosition[5] = {0,0,0,0,0};
+  pendulum(myPosition);
+  Serial.println("Ready to begin procedure...");
+}
+
 void end_serial(){
  Dynamixel.end();
  set_torque(false);
 }
 
 int getpos(int ID){
-  return ( Dynamixel.getPosition(ID));
+	unsigned char help = (unsigned char) ID;
+  return ( Dynamixel.getPosition(help));
 }
 
-void move(int ID1, int ID2, int ID3, int ID4, int ID5){
-  Dynamixel.setNGoalPositions(ID1,ID2,ID3,ID4,ID5);
-  // TODO: add function to wait until finished
+void move(int arr[]){
+  Dynamixel.setNGoalPositions(arr[0],arr[1],arr[2],arr[3],arr[4]);
 }
 
-int avgArr(int arr[5]){
-  int n,sum,avg;
-  n = 5;
-    for (int i = 0; i<n; i++){
-        sum += arr[i];
-    }
-  avg = sum/n;
-  return avg;
-}
-
-void pendulum(int goal1, int goal2, int goal3, int goal4, int goal5){
-  int current_pos[5];
-  int target_1[5] = {goal1,goal2,goal3,goal4,goal5};
-  int target_2[5] = {goal1 + 500,goal2 + 500,goal3 + 500,goal4,goal5};
-  // Get the current positions
-  current_pos[0] = getpos(1);
-  current_pos[1] = getpos(2);
-  current_pos[2] = getpos(3);
-  current_pos[3] = getpos(4);
-  current_pos[4] = getpos(5);
-
-  int loop = 1;
-  int avg1, avg2;
-
-  while (loop > 0){
-    avg1 = avgArr(current_pos);
-    avg2 = avgArr(target_1);
-    Serial.println(avg1);
-    Serial.println(avg2);
-    move(target_1[0],target_1[1],target_1[2],target_1[3],target_1[4]);
-    if (getpos(1) == current_pos[0] && getpos(2) == current_pos[1] && getpos(3)){
-      // TODO: This if statement is very unfinished
-      loop = 0;
-    }
+boolean positionCompare(int arr[],int brr[]){
+  int a = 0 ;
+  for(int i = 0 ; i < 5; i++){
+    Serial.print("Iteration of i: "); Serial.println(i);
+	Serial.print("Desired: "); Serial.println(arr[i]);
+	Serial.print("Current: "); Serial.println(brr[i]);
+	
+	if(arr[i]+100 > brr[i] && arr[i]-100 <brr[i]){
+	  a = 1+a;
+	}
+	else{
+	}
+	Serial.print("Value of a: "); Serial.println(a); Serial.println("");
   }
-  move(target_2[0],target_2[1],target_2[2],target_2[3],target_2[4]);
-  getpos(1);
-  getpos(2);
-  getpos(3);
-  getpos(4);
-  getpos(5);
-  Serial.println("hej");
+  Serial.println("");
+  if(a == 5){
+    return false ;
+  }
+  else{
+    return true ;
+  }
+} 
 
+void pendulum(int arr[]){
+  arr[1] = (-(arr[1])+90);
+  arr[2] = (-arr[2]);
+  arr[4] = (-arr[4]);
+  int desired_pos[5] = {deg(arr[0]),deg(arr[1]),deg(arr[2]),deg(arr[3]),deg(arr[4])};
+  //Need to add something which checks if the postion is possible
+  int current_pos[5];
+
+  do{
+	for(int i = 0 ; i < 5 ; i++){
+	  current_pos[i] = getpos(i+1);
+	}
+    move(desired_pos);
+	}
+	while(positionCompare(desired_pos,current_pos));
+}
+
+int deg(int a){
+  int b = map(a,-180,180,0,4095);
+  return b;
+}
+
+void inverse_kine(int x, int y, int z){
+  double b = 17.66, L1 = 21.98, L2 = 26.428 ;
+  double q1 = PI / 0.2e1 - acos((z - b) * pow(x * x + y * y + pow(z - b, 0.2e1), -0.1e1 / 0.2e1));
+  double q2 = acos((L1 * L1 - L2 * L2 + x * x + y * y + pow(z - b, 0.2e1)) / L1 * pow(x * x + y * y + pow(z - b, 0.2e1), -0.1e1 / 0.2e1) / 0.2e1);
+  double q3 = acos((L2 * L2 + L1 * L1 - x * x - y * y - pow(z - b, 0.2e1)) / L1 / L2 / 0.2e1);
+
+  //Deriving the first angle 
+  double theta1 = atan2(y, x);
+  // Deriving the second angle
+  double theta2 = q1+q2;
+  // Deriving the third angle
+  double theta3 = q3 - PI;
+  
+  int ang1 = (int)(theta1*180)/PI;
+  int ang2 = (int)(theta2*180)/PI;
+  int ang3 = (int)(theta3*180)/PI;
+
+  Serial.print("Angles: ");
+  Serial.print(" 1. "); Serial.print(ang1);
+  Serial.print(" 2. "); Serial.print(ang2);
+  Serial.print(" 3. "); Serial.println(ang3);
+  
+  int positionS[5] = {ang1,ang2,ang3,0,0};
+  pendulum(positionS);
+
+}
+
+int translateIR(){ // takes action based on IR code received
+  switch(results.value){
+  case 0xFFA25D: Serial.println("POWER");  button_pressed = POWER; 
+  power = !ON;
+  if(power == false){
+    end_serial();
+  }
+  else if(power == true){
+    begin_serial();
+  }
+  break;
+  case 0xFF629D: Serial.println("VOL+"); button_pressed = VOLP; 
+  x += 5;
+  inverse_kine(x,y,z);
+  break;
+  case 0xFFE21D: Serial.println("FUNC/STOP"); button_pressed = FUNC; 
+  
+  break;
+  case 0xFF22DD: Serial.println("LEFT"); button_pressed = LEFT;  
+  y += 5;
+  inverse_kine(x,y,z);
+  break;
+  case 0xFF02FD: Serial.println("PAUSE/START"); button_pressed = PAUSE; 
+  
+  break;
+  case 0xFFC23D: Serial.println("RIGHT"); button_pressed = RIGHT; 
+  y -= 5;
+  inverse_kine(x,y,z);
+  break;
+  case 0xFFE01F: Serial.println("DOWN"); button_pressed = DOWN; 
+  z -= 5;
+  inverse_kine(x,y,z);
+  break;
+  case 0xFFA857: Serial.println("VOL-"); button_pressed = VOLM; 
+  x -= 5;
+  inverse_kine(x,y,z);
+  break;
+  case 0xFF906F: Serial.println("UP"); button_pressed = UP; 
+  z += 5;
+  inverse_kine(x,y,z);
+  break;
+  case 0xFF6897: Serial.println("0"); button_pressed = ZERO; 
+  
+  break;
+  case 0xFF9867: Serial.println("EQ"); button_pressed = EQ; 
+  
+  break;
+  case 0xFFB04F: Serial.println("REPEAT"); button_pressed = REPEAT; 
+  
+  break;
+  case 0xFF30CF: Serial.println("1"); button_pressed = ONE; 
+  
+  break;
+  case 0xFF18E7: Serial.println("2"); button_pressed = TWO; 
+  
+  break;
+  case 0xFF7A85: Serial.println("3"); button_pressed = THREE; 
+  
+  break;
+  case 0xFF10EF: Serial.println("4"); button_pressed = FOUR; 
+  
+  break;
+  case 0xFF38C7: Serial.println("5"); button_pressed = FIVE; 
+  
+  break;
+  case 0xFF5AA5: Serial.println("6"); button_pressed = SIX; 
+  
+  break;
+  case 0xFF42BD: Serial.println("7"); button_pressed = SEVEN; 
+  
+  break;
+  case 0xFF4AB5: Serial.println("8"); button_pressed = EIGHT; 
+  
+  break;
+  case 0xFF52AD: Serial.println("9"); button_pressed = NINE; 
+  
+  break;
+  case 0xFFFFFFFF: Serial.println("SAME AS BEFORE");
+  
+  break;  
+  }// End Case
+  Serial.println("...Desired position reached...");
+  Serial.println("Awaiting new order...");
+  return button_pressed;
+} 
+  
+void Callback(const std_msgs::UInt16MultiArray& emg){
+  int emg_array[8];
+    for(int i=0; i<8; i++){
+      emg_array[i] = emg.data[i];
+    }
+}
+
+ros::NodeHandle nh;
+ros::Subscriber<std_msgs::UInt16MultiArray> sub("emg_data/mat_emg", Callback);
+
+void setup(){
+  nh.initNode();
+  nh.subscribe(sub);
+  Serial.begin(57600);                                  // Start serial communication on baudrate 57600
+  irrecv.enableIRIn();
+  begin_serial();
 }
 
 void loop(){
   nh.spinOnce();
-  delay(1);
-  //pendulum(1500, 2045, 2045, 2045, 2045);
-  // end_serial();
+  
+  if (irrecv.decode(&results)){ // have we received an IR signal?
+	//Serial.println(results.value, HEX);
+    int myVal = translateIR();
+	//Serial.println(myVal);	
+    irrecv.resume(); // receive the next value
+  }
+  
+  delay(500); // Do not get immediate repeat
 }
