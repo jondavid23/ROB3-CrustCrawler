7f2e365645df8865f0e1719991d9bfe48451c43b
LarsGS <larsgs@live.dk>
2017-11-07 15:22:19 +0100

    Add files via upload

---
+++ Arduino-CrustcrawlerL/myArduino.h
@@ -0,0 +1,16 @@
+#ifndef myArduino_h
+#define myArduino_h
+
+#include "Arduino.h"
+
+class myArduino
+{
+  public:
+    int deg(int a);
+    boolean positionCompare(int arr[],int brr[]);
+    void pendulum(int arr[]);
+  private:
+    
+};
+
+#endif myArduino_h
